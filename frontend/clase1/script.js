10278827d528f72222b82c7f54c699bd85e1d14f
neolabar <dante.panella.contractor@gdevtools.com>
2020-10-21 18:45:58 -0300

    Actualizado en Develop

--- frontend/clase1/script.js
+++ frontend/clase1/script.js
@@ -5,12 +5,12 @@ const tmp = document.createElement('template');
 
 // definimos el innerHtml --> donde le agregamos los elementos 
 // aqui adentro definimos todos los elementos que interacturan 
-tmp.innerHTML= `
+tmp.innerHTML = `
     <div>
         <input type="text" placeholder="ingrese texto" /> <br />
         <label miPropiedad="" />
     </div>    
-`; 
+`;
 
 // definimos la clase 
 class MiElemento extends HTMLElement {
@@ -19,11 +19,16 @@ class MiElemento extends HTMLElement {
     constructor() {
         super();
 
-        this._shadowRoot = this.attachShadow({'mode':'open'});
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
         this._shadowRoot.appendChild(tmp.content.cloneNode(true));
+        this.lbl = this._shadowRoot.querySelector('[miPropiedad]')
+        this.inp = this._shadowRoot.querySelector('input');
+        this.inp.addEventListener('input', () => {
+            this.lbl.innerHTML = this.inp.value
+        })
 
     }
 }
 
 // para crear el elemento y mostrarlo en la pagina html
-window.customElements.define('neo-input',MiElemento);
+window.customElements.define('neo-input', MiElemento);
