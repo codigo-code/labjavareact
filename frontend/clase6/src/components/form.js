477966dd7632c5f218e0e4f92e025f256506dc63
Neoris <Neoris>
2020-11-20 19:04:56 -0300

    update obj useStateupdate obj useState

--- frontend/clase6/src/components/form.js
+++ frontend/clase6/src/components/form.js
@@ -4,27 +4,32 @@ import React, { useState } from 'react';
 
 
  const NeoForm = () => {
-  const [user, setUser] = useState('');
+  const [name, setUser] = useState('');
   const [password, setPassword] = useState('');
+  const [obj, setObj ] = useState({});
 
     return (
         <React.Fragment>
 
             <input type="text"
              onChange={(e)=>{
-                setUser(e.target.value);
+                // setUser(e.target.value);
+                setObj({...obj,[name]:e.target.value});
             }} placeholder="user" /> <br />
             <input type="text" 
              onChange={(e)=>{
-                setPassword(e.target.value);
+                // setPassword(e.target.value);
+               
+                
+                setObj({...obj,[password]:e.target.value});
             }}
             placeholder="password" /> <br />
             <button onClick={()=>{
-                const data = {
-                    name: user,
-                    password: password
-                };
-                axios.post('http://localhost:8082/login',data).then(callback=>{
+                // const data = {
+                //     name: user,
+                //     password: password
+                // };
+                axios.post('http://localhost:8082/login',obj).then(callback=>{
                     console.log(callback);
                 });
             }}>Validar</button>
